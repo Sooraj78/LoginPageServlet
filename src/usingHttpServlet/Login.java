cc0043e7bbe19e27b2288d99ef4ee1d128d60473
Sooraj79 <soorajgowda540@gmail.com>
2023-04-04 14:46:57 +0530

    LoginPage

---
+++ src/usingHttpServlet/Login.java
@@ -0,0 +1,13 @@
+package usingHttpServlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login extends HttpServlet
+{
+	protected void doGet(HttpServletRequest req, HttpServletResponse res)
+	{
+			 
+	}
+}
